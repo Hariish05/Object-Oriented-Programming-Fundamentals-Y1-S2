84771e51cf120d15408171577bd500f0090ce6e1
WillyHao <jhphan1641@gmail.com>
2025-07-26 22:57:17 +0800

    Added placeholder Battle.java

---
+++ Final Semester Project/src/Battle.java
@@ -0,0 +1,46 @@
+
+public class Battle {
+
+	private Pokemon opponent_pokemon;
+	private Pokemon player_pokemon;
+	
+	public Battle() {
+	}
+
+	public Battle(Pokemon opponent_pokemon, Pokemon player_pokemon) {
+		this.opponent_pokemon = opponent_pokemon;
+		this.player_pokemon = player_pokemon;
+	}
+
+	public Pokemon getOpponent_pokemon() {
+		return opponent_pokemon;
+	}
+
+	public void setOpponent_pokemon(Pokemon opponent_pokemon) {
+		this.opponent_pokemon = opponent_pokemon;
+	}
+
+	public Pokemon getPlayer_pokemon() {
+		return player_pokemon;
+	}
+
+	public void setPlayer_pokemon(Pokemon player_pokemon) {
+		this.player_pokemon = player_pokemon;
+	}
+
+	public void startBattle() {
+		
+	}
+	
+	public void Loser() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Battle [opponent_pokemon=%s, player_pokemon=%s]", opponent_pokemon, player_pokemon);
+	}
+	
+	
+	
+}
