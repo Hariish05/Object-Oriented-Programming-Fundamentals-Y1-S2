00c79c98616a72d107b563ede456d3cbaaad3cb5
WillyHao <jhphan1641@gmail.com>
2025-08-04 20:37:08 +0800

    update damage

--- Final Semester Project/src/Water.java
+++ Final Semester Project/src/Water.java
@@ -7,9 +7,9 @@ public class Water extends Pokemon{
     }
 
     // Water Pokemon object creation (VALUES NOT FINAL)
-    public static Pokemon squirtle = new Water("Squirtle",3,0,0,"Squirtle",100,ZMoves.HYDRO_VORTEX_SQUIRTLE);
-    public static Pokemon oshawott = new Water("Oshawott",4,0,0,"Oshawott",100,ZMoves.HYDRO_VORTEX_OSHAWOTT);
-    public static Pokemon piplup = new Water("Piplup",2,0,0,"Piplup",100,ZMoves.HYDRO_VORTEX_PIPLUP);
+    public static Pokemon squirtle = new Water("Squirtle",5,0,0,"Squirtle",100,ZMoves.HYDRO_VORTEX_SQUIRTLE);
+    public static Pokemon oshawott = new Water("Oshawott",7,0,0,"Oshawott",100,ZMoves.HYDRO_VORTEX_OSHAWOTT);
+    public static Pokemon piplup = new Water("Piplup",7,0,0,"Piplup",100,ZMoves.HYDRO_VORTEX_PIPLUP);
 
     public static List<Pokemon> getAllWaterPokemon(){
         return Arrays.asList(squirtle,oshawott,piplup);
