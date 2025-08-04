7c8e1185d99e235aa5722df85a70f34432b15dc6
Hariish <hariishraoparanthaman@gmail.com>
2025-08-04 20:15:30 +0800

    trying file manager for player

--- Final Semester Project/src/driverFile.java
+++ Final Semester Project/src/driverFile.java
@@ -1,18 +1,20 @@
 public class driverFile {
-    public static void main(String[] args)  {
-        //THIS HAS TO BE DONE AT THE START EVERYTIME
+    public static void main(String[] args) {
+        // THIS HAS TO BE DONE AT THE START EVERYTIME
         FileManager.createAllPokemonTxt();
-        //TESTS
-        Grass.bulbasuar.setHp(Grass.bulbasuar.getMaxHp()-2);
+        // TESTS
+        Grass.bulbasuar.setHp(Grass.bulbasuar.getMaxHp() - 2);
         System.out.println(Grass.bulbasuar.toString());
         boolean catchStatus = Pokeball.attemptCatch(Pokeball.POKEBALL);
         if (catchStatus) {
             System.out.println("Pokemon has been caught!");
-        } else{
+        } else {
             System.out.println("Pokemon has slipped away!");
         }
-        Player dummy = new Player("yur");
+        Player dummy = new Player("Test_Player");
         Player.chooseStarter(dummy);
-
+        System.out.println(dummy.playerData());
+        FileManager playerFileManager = new FileManager();
+        playerFileManager.playerTextFile(dummy);
     }
 }
