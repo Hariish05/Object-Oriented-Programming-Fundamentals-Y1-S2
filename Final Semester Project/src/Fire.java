bfb99009ea95169b2cb10da4d2b6f949bc05eb41
WillyHao <jhphan1641@gmail.com>
2025-08-04 20:36:08 +0800

    update damage

--- Final Semester Project/src/Fire.java
+++ Final Semester Project/src/Fire.java
@@ -7,9 +7,9 @@ public class Fire extends Pokemon{
         super(nickname, maxHp, atk, def, speed, species,ZMove);
     }
     //Fire Pokemon object creation (VALUES NOT FINAL)
-    public static Pokemon charmander = new Fire("Charmander",2,0,0,"Charmander",100,ZMoves.INFERNO_OVERDRIVE_CHARMANDER);
-    public static Pokemon torchic = new Fire("Torchic",2,0,0,"Torchic",100,ZMoves.INFERNO_OVERDRIVE_TORCHIC);
-    public static Pokemon tepig = new Fire("Tepig",3,0,0,"Tepig",100,ZMoves.INFERNO_OVERDRIVE_TEPIG);
+    public static Pokemon charmander = new Fire("Charmander",5,0,0,"Charmander",100,ZMoves.INFERNO_OVERDRIVE_CHARMANDER);
+    public static Pokemon torchic = new Fire("Torchic",6,0,0,"Torchic",100,ZMoves.INFERNO_OVERDRIVE_TORCHIC);
+    public static Pokemon tepig = new Fire("Tepig",6,0,0,"Tepig",100,ZMoves.INFERNO_OVERDRIVE_TEPIG);
 
     public static List<Pokemon> getAllFirePokemon(){
         return Arrays.asList(charmander,torchic,tepig);
